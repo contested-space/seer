d1f7cead304c3b827c2fe776cf1c2b9ad96f0388
Fabien Lamarche-Filion <fabien.lf@samsung.com>
2020-01-27 20:22:36 -0500

    Cosmetic changes

--- src/seer.erl
+++ src/seer.erl
@@ -27,14 +27,6 @@ dist_record(Dist, Val) ->
     N = atomics:add_get(Ref, ?DIST_RESERVOIR_SIZE + 1, 1),
     dist_record2(Ref, Val, N).
 
-dist_record2(Ref, Val, N) when N =< ?DIST_RESERVOIR_SIZE ->
-    atomics:put(Ref, N, Val);
-dist_record2(Ref, Val, N) ->
-    case granderl:uniform(N) of
-        X when X < ?DIST_RESERVOIR_SIZE -> atomics:put(Ref, X, Val);
-        _ -> ok
-    end.
-
 -spec gauge_set(binary(), integer()) -> ok.
 gauge_set(Gauge, Val) ->
     {ok, Ref} = get_or_new(gauge, Gauge),
@@ -44,32 +36,6 @@ read(Type, Name) ->
     {ok, Ref} = get(Type, Name),
     {ok, read_metric(Type, Name, Ref)}.
 
-read_metric(counter, _Name, Ref) -> atomics:exchange(Ref, 1, 0);
-read_metric(gauge, _Name, Ref) -> atomics:get(Ref, 1);
-read_metric(dist, Name, Ref) ->
-    new(dist, Name),
-    N = atomics:get(Ref, ?DIST_RESERVOIR_SIZE + 1),
-    Samples =
-        [
-            atomics:get(Ref, Idx)
-            || Idx <- lists:seq(1, min(N, ?DIST_RESERVOIR_SIZE))
-        ],
-    SortedSamples = lists:sort(Samples),
-    [Max, P99, P90, P50, Min] =
-        case SortedSamples of
-            [] -> [0, 0, 0, 0, 0];
-            _ ->
-                nths(summary_stats_idxs(N, ?DIST_RESERVOIR_SIZE), SortedSamples)
-        end,
-    #{
-        n_samples => N,
-        min => Min,
-        max => Max,
-        p50 => P50,
-        p90 => P90,
-        p99 => P99
-    }.
-
 read_all() ->
     [
         {Type, Name, read_metric(Type, Name, Ref)}
@@ -77,6 +43,14 @@ read_all() ->
     ].
 
 % private
+dist_record2(Ref, Val, N) when N =< ?DIST_RESERVOIR_SIZE ->
+    atomics:put(Ref, N, Val);
+dist_record2(Ref, Val, N) ->
+    case granderl:uniform(N) of
+        X when X < ?DIST_RESERVOIR_SIZE -> atomics:put(Ref, X, Val);
+        _ -> ok
+    end.
+
 get_or_new(Type, Name) ->
     case get(Type, Name) of
         {ok, Ref} -> {ok, Ref};
@@ -106,11 +80,6 @@ new(dist, Name) ->
     persistent_term:put({?MODULE, Name}, {dist, Ref}),
     Ref.
 
-%% nths([], _N, _Lst, Res) -> Res;
-%% nths([Idx | IdxRest], Idx, [Elt | LstRest], Res) ->
-%%     nths(IdxRest, Idx + 1, LstRest, [Elt | Res]);
-%% nths(Idxs, Idx, Lst, Res) -> nths(Idxs, Idx + 1, tl(Lst), Res).
-
 nths(Idxs, Lst) -> nths(Idxs, 1, Lst, []).
 
 nths([], _N, _Lst, Res) -> Res;
@@ -118,6 +87,33 @@ nths([Idx | IdxRest], Idx, Lst, Res) ->
     nths(IdxRest, Idx, Lst, [hd(Lst) | Res]);
 nths(Idxs = [Idx | _], Idx2, Lst, Res) when Idx > Idx2 ->
     nths(Idxs, Idx2 + 1, tl(Lst), Res).
+
+read_metric(counter, _Name, Ref) -> atomics:exchange(Ref, 1, 0);
+read_metric(gauge, _Name, Ref) -> atomics:get(Ref, 1);
+read_metric(dist, Name, Ref) ->
+    new(dist, Name),
+    N = atomics:get(Ref, ?DIST_RESERVOIR_SIZE + 1),
+    Samples =
+        [
+            atomics:get(Ref, Idx)
+            || Idx <- lists:seq(1, min(N, ?DIST_RESERVOIR_SIZE))
+        ],
+    SortedSamples = lists:sort(Samples),
+    [Max, P99, P90, P50, Min] =
+        case SortedSamples of
+            [] -> [0, 0, 0, 0, 0];
+            _ ->
+                nths(summary_stats_idxs(N, ?DIST_RESERVOIR_SIZE), SortedSamples)
+        end,
+    #{
+        n_samples => N,
+        min => Min,
+        max => Max,
+        p50 => P50,
+        p90 => P90,
+        p99 => P99
+    }.
+
 
 summary_stats_idxs(NumSamples, Size) when NumSamples < Size ->
     summary_stats_idxs(NumSamples, NumSamples);
