893bf655641f71b3390d9f3a91cae7f373c0b91f
Fabien Lamarche-Filion <fabien.lf@samsung.com>
2020-03-15 13:18:39 -0400

    Connect to tcp socket during init step

--- src/seer_server.erl
+++ src/seer_server.erl
@@ -21,53 +21,80 @@
     {
         mode :: undefined | stdout,
         prefix :: undefined | binary(),
-        host :: undefined | binary()
+        host :: undefined | binary(),
+        tcp_socket :: undefined | gen_tcp:socket(),
+        interval :: undefined | non_neg_integer()
     }
 ).
 
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init(_) ->
-    Mode = ?ENV(?ENV_MODE, ?DEFAULT_MODE),
-    Prefix = ?ENV(?ENV_PREFIX, ?DEFAULT_PREFIX),
-    Host = ?ENV(?ENV_HOST, ?DEFAULT_HOST),
-    Interval = ?ENV(?ENV_INTERVAL, ?DEFAULT_INTERVAL),
-    Msg =
-        case Mode of
-            stdout -> poll_stdout;
-            carbon -> poll_carbon
-        end,
-    timer:send_interval(Interval, Msg),
-    {ok, #state{mode = Mode, prefix = Prefix, host = Host}, 0}.
+    InitialState =
+        #state{
+            mode = ?ENV(?ENV_MODE, ?DEFAULT_MODE),
+            prefix = ?ENV(?ENV_PREFIX, ?DEFAULT_PREFIX),
+            host = ?ENV(?ENV_HOST, ?DEFAULT_HOST),
+            interval = ?ENV(?ENV_INTERVAL, ?DEFAULT_INTERVAL)
+        },
+    {Interval, Msg, SetupState} = setup(InitialState),
+    timer:send_after(Interval, Msg),
+    {ok, SetupState, 0}.
 
 handle_call(_, _From, State) -> {reply, {error, undefined_call}, State}.
 
 handle_cast(stop, State) -> {stop, normal, State};
 handle_cast(_, State) -> {noreply, State}.
 
-handle_info(poll_stdout, State) ->
+handle_info(poll_stdout, #state{interval = Interval} = State) ->
     Metrics = seer:read_all(),
     io:format("~w~n", [Metrics]),
+    timer:send_after(Interval, poll_stdout),
     {noreply, State};
-handle_info(poll_carbon, #state{prefix = Prefix, host = Host} = State) ->
+handle_info(
+    poll_carbon,
+    #state{
+            prefix = Prefix,
+            host = Host,
+            tcp_socket = Socket,
+            interval = Interval
+        }
+        =
+        State
+) ->
     Metrics = seer:read_all(),
     CarbonStrings = seer_utils:carbon_format(Prefix, Host, Metrics),
-    case
-    gen_tcp:connect(
-        ?ENV(?ENV_CARBON_HOST, ?DEFAULT_CARBON_HOST),
-        ?ENV(?ENV_CARBON_PORT, ?DEFAULT_CARBON_PORT),
-        [binary]
-    ) of
-        {ok, Socket} ->
-            [gen_tcp:send(Socket, Metric) || Metric <- CarbonStrings],
-            gen_tcp:close(Socket);
-        {error, Reason} ->
-            % TODO: put metrics in buffer and loop for connection
-            io:format("~s~n", [Reason])
-    end,
+    [gen_tcp:send(Socket, Metric) || Metric <- CarbonStrings],
+    timer:send_after(Interval, poll_carbon),
+    % TODO: put metrics in buffer and loop for connection
     {noreply, State};
 handle_info(_, State) -> {noreply, State}.
 
 terminate(_Reason, _State) -> ok.
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+% private
+setup(#state{mode = Mode, interval = Interval} = State) ->
+    {Msg, SetupState} =
+        case Mode of
+            stdout -> {poll_stdout, State};
+            carbon ->
+                {ok, Socket} = get_carbon_socket(),
+                {poll_carbon, State#state{tcp_socket = Socket}}
+        end,
+    {Interval, Msg, SetupState}.
+
+get_carbon_socket() ->
+    case
+    gen_tcp:connect(
+        ?ENV(?ENV_CARBON_HOST, ?DEFAULT_CARBON_HOST),
+        ?ENV(?ENV_CARBON_PORT, ?DEFAULT_CARBON_PORT),
+        [binary]
+    ) of
+        {ok, Socket} -> {ok, Socket};
+        {error, Reason} ->
+            io:format("~s~n", [Reason]),
+            timer:sleep(500),
+            get_carbon_socket()
+    end.
