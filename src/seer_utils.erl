bad71506173264fb02742393c7725df9555baae8
Fabien Lamarche-Filion <fabien.lf@samsung.com>
2020-04-08 08:09:16 -0400

    Remove function-type ambiguity for carbon_string()

--- src/seer_utils.erl
+++ src/seer_utils.erl
@@ -7,11 +7,11 @@
 -spec carbon_format(binary(), binary(), [read_metric()]) -> carbon_batch().
 carbon_format(Prefix, Host, Metrics) ->
     Timestamp = erlang:system_time(second),
-    lists:flatten([carbon_string(Prefix, Host, Metric, Timestamp) || Metric <- Metrics]).
+    lists:flatten([format_carbon_line(Prefix, Host, Metric, Timestamp) || Metric <- Metrics]).
 
 % private
--spec carbon_string(binary(), binary(), read_metric(), integer()) -> carbon_string() | carbon_batch().
-carbon_string(Prefix, Host, {Type, Name, Value}, Timestamp) ->
+-spec format_carbon_line(binary(), binary(), read_metric(), integer()) -> carbon_string() | carbon_batch().
+format_carbon_line(Prefix, Host, {Type, Name, Value}, Timestamp) ->
     MetricKey = metric_key(Prefix, Host, Name),
     TimestampBin = integer_to_binary(Timestamp),
     case Value of
@@ -20,21 +20,25 @@ carbon_string(Prefix, Host, {Type, Name, Value}, Timestamp) ->
       _ ->
           case Type of
             counter ->
-                carbon_string(MetricKey, integer_to_binary(Value), TimestampBin);
+                format_carbon_line(MetricKey, integer_to_binary(Value), TimestampBin);
             gauge ->
-                carbon_string(MetricKey, integer_to_binary(Value), TimestampBin);
+                format_carbon_line(MetricKey, integer_to_binary(Value), TimestampBin);
             dist ->
-                [carbon_string(<<MetricKey/binary, ".", (atom_to_binary(Key, latin1))/binary>>,
+                [format_carbon_line(<<MetricKey/binary, ".", (atom_to_binary(Key, latin1))/binary>>,
                                integer_to_binary(Val),
                                TimestampBin)
                  || {Key, Val} <- maps:to_list(Value)];
             histo ->
                 #{n_samples := NSamples, percentiles := Percentiles} = Value,
-                [carbon_string(<<MetricKey/binary, ".n_samples">>, integer_to_binary(NSamples), TimestampBin)
-                 | [carbon_string(<<MetricKey/binary, ".", Key/binary>>, Val, TimestampBin)
+                [format_carbon_line(<<MetricKey/binary, ".n_samples">>, integer_to_binary(NSamples), TimestampBin)
+                 | [format_carbon_line(<<MetricKey/binary, ".", Key/binary>>, Val, TimestampBin)
                     || {Key, Val} <- make_key_values(Percentiles)]]
           end
     end.
+
+-spec format_carbon_line(binary(), binary(), binary()) -> carbon_string().
+format_carbon_line(MetricKey, Value, Timestamp) ->
+    <<MetricKey/binary, " ", Value/binary, " ", Timestamp/binary, "\n">>.
 
 -spec make_key_values(#{histo_percentile() => histo_bucket_key()} | #{}) -> [{binary(), binary()}].
 make_key_values(Percentiles) ->
@@ -44,11 +48,6 @@ make_key_values(Percentiles) ->
 make_key_val({Key, {_Min, Max}}) ->
     {atom_to_binary(Key, latin1), integer_to_binary(Max)}.
 
--spec carbon_string(binary(), binary(), binary()) -> carbon_string().
-carbon_string(MetricKey, Value, Timestamp) ->
-    <<MetricKey/binary, " ", Value/binary, " ", Timestamp/binary, "\n">>.
-
 -spec metric_key(binary(), binary(), metric_name()) -> binary().
 metric_key(Prefix, Host, Name) ->
     <<Prefix/binary, ".", Host/binary, ".", Name/binary>>.
-
